fd5c2398e7e70986807dc636b741ae1e8725aa9e
wonlee6 <wonlee6@gmail.com>
2021-04-12 16:32:34 +0900

    map,filter

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import Counter from './Counter';
-import EventPractice from './EventPractice';
-import EventPractice2 from './EventPractice2';
-import MyComponent from './MyComponent';
+import React from "react";
+import Counter from "./Counter";
+import EventPractice from "./EventPractice";
+import EventPractice2 from "./EventPractice2";
+import IterationSample from "./IterationSample";
+import MyComponent from "./MyComponent";
 
 const App = () => {
   return (
@@ -18,6 +19,9 @@ const App = () => {
       <>
         <EventPractice />
         <EventPractice2 />
+      </>
+      <>
+        <IterationSample />
       </>
     </>
   );
